87f214dd9cfb4d2fa9b01d8754a5b1f95ededbe8
deakolt <alexandervaccaro@gmail.com>
2021-03-22 14:06:04 -0700

    RPG seeding working

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,48 +1,4 @@
-import prisma from '../client'
-import seedCount from './count'
-import seedRpg from './rpg'
-
-console.log('starting clean and seed')
-
-console.log('wiping Rpg and Count tables')
-wipe()
-console.log('wiped')
-
-console.log('seeding RPG table')
-seedRpg()
-console.log('seeded')
-
-console.log('seeding Count table')
-seedCount()
-console.log('seeded')
-
-console.log('finish clean and seed')
-
-async function wipe() {
-	// TODO : Prisma cascading deletes?
-	await prisma.count.deleteMany({})
-	await prisma.rpg.deleteMany({})
-}
-
-const countsPerRpg: number = 10
-const minSubscribers: number = 1000
-const maxSubscribers: number = 1000000
-
-export default async function seedCount() {
-	const rpgs: Rpg[] = await prisma.rpgs()
-
-	rpgs.forEach(async rpg => {
-		for (let i = 0; i < countsPerRpg; i++) {
-			await prisma.rpg.create({
-				data: {
-					count: Math.floor(Math.random() * (minSubscribers + maxSubscribers) / 2),
-					rpg: rpg
-				}
-			})
-		}
-	})
-}
-
+import prisma from './client'
 
 const rpgs = [
 	{ name: 'Call of Cthulhu', subreddit: 'callofcthulhu' },
@@ -50,6 +6,26 @@ const rpgs = [
 	{ name: 'Mouse Guard', subreddit: 'MouseGuard' },
 	{ name: 'Warhammer Fantasy Roleplaying', subreddit: 'warhammerfantasyrpg' },
 ]
+
+const countsPerRpg: number = 10
+const minSubscribers: number = 1000
+const maxSubscribers: number = 1000000
+
+
+async function main() {
+	console.log('starting seed')
+
+	console.log('seeding RPG table')
+	seedRpg()
+	console.log('seeded')
+
+	console.log('seeding Count table')
+	// seedCount()
+	console.log('seeded')
+
+	console.log('finished seed')
+
+}
 
 export default function seedRpg() {
 	rpgs.forEach(async rpg => {
@@ -61,6 +37,21 @@ export default function seedRpg() {
 		})
 	})
 }
+
+// export default async function seedCount() {
+// 	const rpgs: Rpg[] = await prisma.rpgs()
+
+// 	rpgs.forEach(async rpg => {
+// 		for (let i = 0; i < countsPerRpg; i++) {
+// 			await prisma.rpg.create({
+// 				data: {
+// 					count: Math.floor(Math.random() * (minSubscribers + maxSubscribers) / 2),
+// 					rpg: rpg
+// 				}
+// 			})
+// 		}
+// 	})
+// }
 
 main()
 	.catch(e => {
--- src/counter.ts
+++ src/counter.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import prisma from './db/client'
+import prisma from '../prisma/client'
 
 async function main() {
 	const allRpgs = await prisma.rpg.findMany()
