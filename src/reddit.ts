1119b65425d2e10bd1c8e4e7ec00334d5afd10e6
deakolt <alexandervaccaro@gmail.com>
2021-03-23 14:27:43 -0700

    Remove all authentication, add getSubredditCount

--- src/reddit.ts
+++ src/reddit.ts
@@ -1,75 +1,20 @@
 import axios from 'axios'
-import dotenv from 'dotenv'
 
 import prisma from '../prisma/client'
 
-class Reddit {
-	accessToken: string = ''
-	clientId: string
-	clientSecret: string
-	password: string
-	username: string
+const USER_AGENT: string = 'web:ttrpgeewhiz:v0.0.0 (by /u/MushOnline)'
 
-	constructor() {
-		const env = dotenv.config()
-
-		this.clientId = process.env['REDDIT_CLIENT_ID'] || ''
-		this.clientSecret = process.env['REDDIT_CLIENT_SECRET'] || ''
-		this.username = process.env['REDDIT_USERNAME'] || ''
-		this.password = process.env['REDDIT_PASSWORD'] || ''
-
-		console.log(process.env['REDDIT_CLIENT_SECRET'])
-		if (!this.clientId || !this.clientSecret || !this.password || !this.username) {
-			throw Error('missing shit')
-		}
-
-		this.getAccessToken()
-	}
-
-	async getAccessToken() {
-		axios.interceptors.request.use(request => {
-		  console.log('Starting Request', JSON.stringify(request, null, 2))
-		  return request
-		})
-
-		// axios.interceptors.response.use(response => {
-		//   // console.log('Response:', JSON.stringify(response, null, 2))
-		//   return response
-		// })
-
-
-		const response = await axios.post(
-			'https://www.reddit.com/api/v1/access_token',
-			{
-				headers: {
-					'User-Agent': 'web:ttrpgeewhiz:v0.0.0 (by /u/MushOnline)'
-				},
-				auth: {
-					username: this.clientId,
-					password: this.clientSecret
-				},
-				data: {
-					grant_type: 'password',
-					username: this.username,
-					password: this.password,
-				},
+async function getSubredditCount(subreddit: string) {
+	const response = await axios.get(
+		`https://www.reddit.com/r/${subreddit}/about/.json`,
+		{
+			headers: {
+				'User-Agent': USER_AGENT
 			}
-		)
-
-
-		console.log(response)
-		if (!response) {
-			throw Error('missing shit')
 		}
+	)
 
-		this.accessToken = response.data['access_token']
-	}
-
-	// async fetchSubredditCount(subreddit: string) {
-	// 	return await
-	// }
+	return response.data['data']['subscribers']
 }
 
-console.log('can you hear me beeyotch')
-
-const asdf = new Reddit()
+export default getSubredditCount
